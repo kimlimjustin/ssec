f07871429ad9561da19c151879377bff63a0f83a
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2022-07-31 19:45:53 +0700

    feat: sidebar links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next';
 import Header from '../components/Header/Header';
+import Sidebar from '../components/Sidebar/Sidebar';
 
 const Home: NextPage = () => {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Sidebar />
+            <span>s</span>
+        </>
+    );
 };
 
 export default Home;
