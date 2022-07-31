b36a4b0201610376bb2009660f1b71c033e7c6b9
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2022-07-31 20:11:32 +0700

    feat: writings component

---
+++ components/Writing/Writing.tsx
@@ -0,0 +1,24 @@
+import styles from './Writing.module.css';
+
+interface IWriting {
+    title: string;
+    createdAt: string;
+    category: string;
+    content: string;
+    thumbnail: string;
+}
+
+const Writing = (props: IWriting) => {
+    return (
+        <div className={styles['writing']}>
+            <div className={styles['writing__meta']}>
+                {props.createdAt} &#8226; {props.category}
+            </div>
+            <h1 className={styles['writing__title']}>{props.title}</h1>
+            <img className={styles['writing__thumbnail']} src={props.thumbnail} alt={props.title} />
+            <div className={styles['writing__content']} dangerouslySetInnerHTML={{ __html: props.content }} />
+        </div>
+    );
+};
+
+export default Writing;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from 'next';
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
+import Writing from '../components/Writing/Writing';
 
 const Home: NextPage = () => {
     return (
         <>
             <Header />
             <Sidebar />
-            <span>s</span>
+            <div className="container">
+                <Writing
+                    title="The Wonder of Science IX-Alpha."
+                    content="Laborum excepteur qui labore in non occaecat ipsum. Excepteur reprehenderit aute ea anim reprehenderit fugiat id culpa. Est nostrud tempor excepteur non velit. Fugiat voluptate mollit ea Lorem ut eu magna ipsum aute id labore adipisicing. Ea consectetur enim ullamco ad aute sit ipsum minim dolore. Aliquip consectetur ad ipsum ad ex sint dolore elit incididunt Lorem eiusmod."
+                    createdAt="32 Februari"
+                    category="News"
+                    thumbnail="https://ssec-sutomo1.weebly.com/uploads/1/0/5/2/10526196/1586681380054_orig.jpg"
+                />
+            </div>
         </>
     );
 };
